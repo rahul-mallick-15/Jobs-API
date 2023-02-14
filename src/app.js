d6fcb1ed91c5f78af63c9403aef88f392e6cd1d5
Rahul Mallick <rahulmallick1115@gmail.com>
2023-02-15 03:52:38 +0530

    removed rate-limiter middleware

--- src/app.js
+++ src/app.js
@@ -28,12 +28,12 @@ const jobsRouter = require("../routes/jobs");
 const notFoundMiddleware = require("../middleware/not-found");
 const errorHandlerMiddleware = require("../middleware/error-handler");
 
-app.use(
-  rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
-  })
-);
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 100, // limit each IP to 100 requests per windowMs
+//   })
+// );
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
