5ec001535fd8c60a849fdb8ce928fb75aaa68315
Rahul Mallick <rahulmallick1115@gmail.com>
2023-02-15 03:07:59 +0530

    added swaggerUI

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("swagger.yaml");
+
 const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
@@ -32,8 +37,13 @@ app.use(
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(xss());
 
 //routes
+app.get("/", (req, res) => {
+  res.send("<h1>Jobs API</h1><a href='/api-docs'>Documentation</a>");
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
@@ -45,9 +55,9 @@ const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
+    // app.listen(port, () => {
+    //   console.log(`Server is listening on port ${port}`);
+    // });
   } catch (error) {
     console.log(error);
   }
